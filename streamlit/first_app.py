2232bb075c4450d56b4600844a8e27c171ca1825
tanimutomo <tanimutomo@gmail.com>
2020-04-14 11:01:32 +0900

    Add streamlit

---
+++ streamlit/first_app.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import time
+
+
+"""
+# My First App
+Here's our first aaattempt at using data to create a table:
+"""
+
+# raw data
+df = pd.DataFrame({
+    "first column": [1, 2, 3, 4],
+    "second column": [10, 20, 30, 40],
+})
+df
+
+# chart
+if st.checkbox("Show dataframe"):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns = ['a', 'b', 'c'],
+    )
+    st.line_chart(chart_data)
+
+# map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'],
+)
+st.map(map_data)
+
+# option selectbox
+option = st.sidebar.selectbox(
+    "Which number do you like best?",
+    df["first column"],
+)
+"You selected:", option
+
+"Starting a long compuutation..."
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f"Iteration {i+1}")
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+"...and now we\'re done!"
