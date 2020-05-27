ea69cf924950c382f9fafec280c8dae74dc89d59
tanimutomo <tanimutomo@gmail.com>
2020-05-27 23:33:56 +0900

    Add terraform example

---
+++ terraform/http_server/main.tf
@@ -0,0 +1,30 @@
+variable "instance_type" {}
+
+resource "aws_instance" "default" {
+  ami = "ami-0f310fced6141e627"
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.default.id]
+  user_data = file("./user_data.sh")
+}
+
+resource "aws_security_group" "default" {
+  name = "ec2"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_dns" {
+  value = aws_instance.default.public_dns
+}
